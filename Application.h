35128b89e29eb6525ff1ff6cf7c0ff855cd83b30
ashrantk <ashrantk@umich.edu>
2016-05-19 15:05:11 +0530

    Update Application.h

--- Application.h
+++ Application.h
@@ -2,8 +2,8 @@
 #define Telegram_Application_h
 
 
-#define API_ID 0000 // you can create new app on my.telegram.org
-#define API_HASH @"" // you can create new app on my.telegram.org
+#define API_ID 25384 // you can create new app on my.telegram.org
+#define API_HASH @"55c18abcde11faad245ec1de322d8cef" // you can create new app on my.telegram.org
 #define HOCKEY_APP_IDENTIFIER @"" // hocheckey app
 #define HOCKEY_APP_COMPANY @""  // hocheckey app
 #define BUNDLE_IDENTIFIER @"ru.keepcoder.Telegram"  // bundle name
